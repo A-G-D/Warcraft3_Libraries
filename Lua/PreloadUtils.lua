6d86d5349f77f48eb74354897e5bfc6845b19366
AGD <aloeverdulay@gmail.com>
2020-06-24 12:05:26 +0800

    ㅤ

--- Lua/PreloadUtils.lua
+++ Lua/PreloadUtils.lua
@@ -1,41 +1,41 @@
 --[[ PreloadUtils.lua v1.0.0 https://www.hiveworkshop.com/threads/lua-preloadutils.325644/
 
-	uses:
-		- Global Initialization: https://www.hiveworkshop.com/threads/lua-global-initialization.317099/
+    uses:
+        - Global Initialization: https://www.hiveworkshop.com/threads/lua-global-initialization.317099/
 
-	API:
+    API:
 
-		function PreloadUnit(arg)
-		function PreloadItem(arg)
-		function PreloadAbility(arg)
-			- Args: rawcode(s) enclosed in FourCC()
+        function PreloadUnit(arg)
+        function PreloadItem(arg)
+        function PreloadAbility(arg)
+            - Args: rawcode(s) enclosed in FourCC()
 
-		function PreloadEffect(arg)
-		function PreloadSound(arg)
-			- Args: string path(s)
+        function PreloadEffect(arg)
+        function PreloadSound(arg)
+            - Args: string path(s)
 
 
-	Sample Usage:
+    Sample Usage:
 
-		PreloadUnit(FourCC('hfoo'))
-		PreloadAbility{ FourCC('A000'), FourCC('A001'), FourCC('A002') }
-		PreloadEffect{ path_1, {path_2, path_3}, path_4 }
+        PreloadUnit(FourCC('hfoo'))
+        PreloadAbility{ FourCC('A000'), FourCC('A001'), FourCC('A002') }
+        PreloadEffect{ path_1, {path_2, path_3}, path_4 }
 
 ]]--
 do
-	local t = {}
-	local dummy
+    local t = {}
+    local dummy
 
-	local function OnPreload(callback, arg)
-		if type(arg) == 'table' then
-			for k, v in pairs(arg) do
-				OnPreload(callback, v)
-			end
-		elseif t[arg] == nil then
-			t[arg = 1 -- prevents redundant preloading
-			callback(arg)
-		end
-	end
+    local function OnPreload(callback, arg)
+        if type(arg) == 'table' then
+            for k, v in pairs(arg) do
+                OnPreload(callback, v)
+            end
+        elseif t[arg] == nil then
+            t[arg = 1 -- prevents redundant preloading
+            callback(arg)
+        end
+    end
 
     local function UnitPreloader(id)
         RemoveUnit(CreateUnit(Player(PLAYER_NEUTRAL_PASSIVE), id, 0, 0, 0))
@@ -49,7 +49,7 @@ do
     local function EffectPreloader(path)
         DestroyEffect(AddSpecialEffectTarget(path, dummy, "origin"))
     end
-	-- Credits to Silvenon for sound preloading method
+    -- Credits to Silvenon for sound preloading method
     local function SoundPreloader(path)
         local s = CreateSound(path, false, false, false, 10, 10, "")
         SetSoundVolume(s, 0)
@@ -58,28 +58,28 @@ do
     end
 
     function PreloadUnit(arg)
-		OnPreload(UnitPreloader, arg)
+        OnPreload(UnitPreloader, arg)
     end
     function PreloadItem(arg)
-		OnPreload(ItemPreloader, arg)
+        OnPreload(ItemPreloader, arg)
     end
     function PreloadAbility(arg)
-		OnPreload(AbilityPreloader, arg)
+        OnPreload(AbilityPreloader, arg)
     end
     function PreloadEffect(arg)
-		OnPreload(EffectPreloader, arg)
+        OnPreload(EffectPreloader, arg)
     end
     function PreloadSound(arg)
-		OnPreload(SoundPreloader, arg)
+        OnPreload(SoundPreloader, arg)
     end
 
-	-- init
-	onGlobalInit(function ()
-		dummy = CreateUnit(Player(PLAYER_NEUTRAL_PASSIVE), FourCC('hpea'), 0, 0, 0)
-		UnitAddAbility(dummy, FourCC('AInv'))
-		UnitAddAbility(dummy, FourCC('Avul'))
-		UnitRemoveAbility(dummy, FourCC('Amov'))
-		SetUnitY(dummy, GetRectMaxY(world) + 1000)
-		RemoveRect(world)
-	end)
+    -- init
+    onGlobalInit(function ()
+        dummy = CreateUnit(Player(PLAYER_NEUTRAL_PASSIVE), FourCC('hpea'), 0, 0, 0)
+        UnitAddAbility(dummy, FourCC('AInv'))
+        UnitAddAbility(dummy, FourCC('Avul'))
+        UnitRemoveAbility(dummy, FourCC('Amov'))
+        SetUnitY(dummy, GetRectMaxY(world) + 1000)
+        RemoveRect(world)
+    end)
 end
